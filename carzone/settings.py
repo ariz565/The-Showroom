6f98ea3a510617c4add902e74d924600c25ba2ca
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-03-09 00:16:29 +0530

    cars application

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -79,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : 'carzone_db',
         'USER' : 'postgres',
-        'PASSWORD': 'your pass here',
+        'PASSWORD': '@Sherwani#0565',
         'HOST' : 'localhost',
         'PORT': '5432',
     }
