ad43e6e166fe636af8ed8a1b4cbd874e7b2b950c
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-03-09 00:22:56 +0530

    cars application

--- carzone/settings.py
+++ carzone/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : 'carzone_db',
         'USER' : 'postgres',
-        'PASSWORD': '@Sherwani#0565',
+        'PASSWORD': '',
         'HOST' : 'localhost',
         'PORT': '5432',
     }
