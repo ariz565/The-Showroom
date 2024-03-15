5b8e5b8709b5aad54af3dfadf6a53acadf1047ca
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-03-16 02:00:35 +0530

    login part reh gya hai

--- pages/views.py
+++ pages/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-from pages.models import Team
-
+from .models import Team
+from cars.models import Car
 
 # Create your views here.
 def home(request):
     teams = Team.objects.all()
-    data = {
-        'teams' : teams
+    featured_cars=Car.objects.order_by('-created_date').filter(is_featured=True)
+    all_cars=Car.objects.order_by('-created_date')
+    data= {
+        'teams':teams,
+        'featured_cars':featured_cars,
+        'all_cars':all_cars,
     }
     return render(request, 'pages/home.html', data)
 
 def about(request):
     teams = Team.objects.all()
-    data = {
-        'teams' : teams
+    data={
+        'teams':teams,
     }
     return render(request, 'pages/about.html',data)
 
